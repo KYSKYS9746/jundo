24b8c1c0770c5f3c149ecad6302c8914becf54b8
KYSKYS9746 <148739304+KYSKYS9746@users.noreply.github.com>
2024-03-04 18:13:21 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,31 +1,62 @@
-$(function(){
+$(function () {
+
+    $(function () {
+        $('.mopen').on('click', function () {
+            $('.gnb').toggleClass('on');
+            $(this).find('.hamburger').toggleClass('is-active');
+        })
+    })
+
     $('.main_visual .main_visual_slide').slick({
         arrows: false,
-        autoplaySpeed: 4000,
-        
-    });
-
-    $('.main_regular .main_regular_slide').slick({
-        slidesToShow: 6,
-        slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 2000,
-        arrows:false,
+        pauseOnHover: false,
+        fade: true,
     });
-    $('.main_event .main_event_slide').slick({
-        slidesToShow: 5,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 3000,
-        arrows:false,
-    });
+
+
 
     $('.main_company .main_company_slide').slick({
         slidesToShow: 9,
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 2000,
-        arrows:false,
+        arrows: false,
+
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 4,
+                    pauseOnHover: false
+                }
+            }
+        ]
     });
+
+    $('.main_regular .content_tab .tab_menu li a').on('click', function (e) {
+        e.preventDefault();
+        const idx = $(this).parent().index();
+        $('.main_regular .content_tab .tab_menu li a').removeClass('on')
+        $('.main_regular .content_tab .tab_menu li a').eq(idx).addClass('on')
+
+        console.log(idx); // 0 , 1 , 2
+        $('.main_regular .tab_content li').removeClass('on');
+        $('.main_regular .tab_content li').eq(idx).addClass('on');
+
+    });
+
+    $(function () {
+        $(window).on('scroll', function () {
+            const scrollHeight = $(window).scrollTop();
+            console.log(scrollHeight);
+            if (scrollHeight > 0) {
+                $('.header').addClass('on')
+            } else {
+                $('.header').removeClass('on')
+            }
+        })
+    })
 
 })
